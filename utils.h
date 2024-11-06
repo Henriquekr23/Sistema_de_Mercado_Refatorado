94bb509c7b0852025667173be28136bad98eb446
Henrique <henriquekr23@gmail.com>
2024-11-06 13:39:10 -0300

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,9 @@
+// utils.h
+#ifndef UTILS_H
+#define UTILS_H
+
+int lerInteiro(const char *mensagem);
+float lerFloat(const char *mensagem);
+void lerString(char *buffer, int tamanho);
+
+#endif // UTILS_H
